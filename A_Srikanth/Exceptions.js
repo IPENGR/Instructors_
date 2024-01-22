e4a4d24ad2343af2c7b5836bfa9c3aace4656705
IPENGR <engr@innova-path.com>
2024-01-22 08:33:48 -0800

    Add files via upload

---
+++ A_Srikanth/Exceptions.js
@@ -0,0 +1,67 @@
+// try {
+//     let result = 10 / 0; 
+//     console.log(result);
+//     console.log(reslt);
+//   } catch (error) {
+//     console.error('An error occurred:', error.name);
+//   } finally {
+//     console.log('Finally block executed');
+//   }
+
+
+// ********************************************************************
+// try catch :
+// try {
+//   let result = add(10, 20);
+//   console.log(result);
+// } catch (e) {
+//   console.log({ name: e.name, message: e.message });
+// }
+// console.log('Bye');
+
+
+// ignoring the catch block
+
+// const add = (x, y) => x + y;
+
+// try {
+//   let result = add(10, 20);
+//   console.log(result);
+// } catch (e) {
+//   console.log({ name: e.name, message: e.message });
+// }
+// console.log('Bye');
+
+
+// **********************************************************************************
+
+
+// try catch finally : 
+
+// let result = 0;
+// try {
+//   result = add(10, 20);
+// } catch (e) {
+//   console.log(e.message);
+// } finally {
+//   console.log({ result });
+// }
+
+
+// const add = (x, y) => x + y;
+
+// let result = 0;
+
+// try {
+//   result = add(10, 20);
+// } catch (e) {
+//   console.log(e.message);
+// } finally {
+//   console.log({ result });
+// }
+
+
+// **********************************************************************************
+
+// throw :
+
