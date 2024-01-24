a4e9506e01ead38d05a0832ad4470bb1e97e67e4
IPENGR <engr@innova-path.com>
2024-01-24 20:52:20 +0530

    Add files via upload

--- ULASA SAKETH/practice2.py
+++ ULASA SAKETH/practice2.py
@@ -1,3 +1,19 @@
-class apple():
-    for i in range(10):
-        print(i)
+from threading import *
+import time
+class apple(Thread):
+    def run(self):
+     for i in range(10):
+        time.sleep(1)
+        print("apple")
+
+class orange(Thread):
+    def run(self):
+      for i in range(10):
+        time.sleep(1)
+        print("orange")
+a=apple()
+b=orange()
+a.start()
+b.start()
+'''a.join()
+b.join()'''
