227c943f28e3051802c004be1dd5a2aef67024e8
IPENGR <engr@innova-path.com>
2024-03-27 09:07:56 -0700

    Add files via upload

---
+++ ULASA SAKETH/palindrome.py
@@ -0,0 +1,5 @@
+a="madam"
+if a[::-1]==a:
+    print("it is a palindome")
+else:
+    print("is not")
---
+++ ULASA SAKETH/str_format.py
@@ -0,0 +1,5 @@
+name="saketh"
+age=23
+city="Hyderabad"
+text="hi ,my name is {} , iam {} years old and iam from {}"
+print(text.format(name,age,city))
